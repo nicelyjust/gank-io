9d137b414452370d354bda96a6ec8a9d5a56111d
lizeng <lizeng@oaserver.dw.gdbbk.com>
2017-12-19 21:06:20 +0800

    网页+;

---
+++ app/src/main/java/com/eebbk/geek/news/view/NewsWebActivity.java
@@ -0,0 +1,17 @@
+package com.eebbk.geek.news.view;
+
+import com.eebbk.geek.R;
+import com.eebbk.geek.base.activities.BaseActivity;
+
+public class NewsWebActivity extends BaseActivity {
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_news_web;
+    }
+
+    @Override
+    protected void initWidget() {
+        super.initWidget();
+    }
+}
