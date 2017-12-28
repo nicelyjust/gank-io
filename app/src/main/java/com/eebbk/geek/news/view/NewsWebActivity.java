7d3f7687f757358fe12174f3dee61c39124ca574
lizeng <lizeng@oaserver.dw.gdbbk.com>
2017-12-28 20:23:09 +0800

    +;

--- app/src/main/java/com/eebbk/geek/news/view/NewsWebActivity.java
+++ app/src/main/java/com/eebbk/geek/news/view/NewsWebActivity.java
@@ -1,9 +1,22 @@
 package com.eebbk.geek.news.view;
 
+import android.support.v7.widget.Toolbar;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
+
 import com.eebbk.geek.R;
 import com.eebbk.geek.base.activities.BaseActivity;
 
+import butterknife.BindView;
+
 public class NewsWebActivity extends BaseActivity {
+
+    @BindView(R.id.toolbar_news_web)
+    Toolbar mToolbar;
+    @BindView(R.id.pb_news_web)
+    ProgressBar mPb;
+    @BindView(R.id.webView_news_web)
+    WebView mWebView;
 
     @Override
     protected int getContentView() {
---
+++ app/src/main/java/com/eebbk/geek/view/TitleItemDecoration.java
@@ -0,0 +1,74 @@
+package com.eebbk.geek.view;
+
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+
+/*
+ *  @项目名：  Pad_VideoTraining 
+ *  @包名：    com.eebbk.vtraining.view
+ *  @文件名:   TitleItemDecoration
+ *  @创建者:   lz
+ *  @创建时间:  2017/12/28 19:39
+ *  @描述：    TODO
+ */
+
+public class TitleItemDecoration extends RecyclerView.ItemDecoration {
+    //设置ItemView的内嵌偏移长度（inset）
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        int position = ((RecyclerView.LayoutParams) view.getLayoutParams()).getViewLayoutPosition();
+
+        if (parent.getLayoutManager() instanceof GridLayoutManager) {
+
+        } else if (parent.getLayoutManager() instanceof LinearLayoutManager && !isLastRow(position , parent)) {
+            outRect.set(0, 0, 0, 10);
+        } else {
+            super.getItemOffsets(outRect, view, parent, state);
+        }
+
+    }
+    // 绘制图层在ItemView以下，所以如果绘制区域与ItemView区域相重叠，会被遮挡
+    @Override
+    public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        super.onDraw(c, parent, state);
+    }
+    // 同样是绘制内容，但与onDraw（）的区别是：绘制在图层的最上层,用于制造悬浮的效果
+    @Override
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        super.onDrawOver(c, parent, state);
+    }
+
+    /**
+     * 是否最后一行
+     */
+    public boolean isLastRow(int itemPosition, RecyclerView parent) {
+        RecyclerView.LayoutManager layoutManager = parent.getLayoutManager();
+        if (layoutManager instanceof GridLayoutManager) {
+            int spanCount = ((GridLayoutManager) layoutManager).getSpanCount();
+            int itemCount = parent.getAdapter().getItemCount();
+            if ((itemCount - itemPosition - 1) < spanCount)
+                return true;
+        } else if (layoutManager instanceof LinearLayoutManager) {
+            return parent.getAdapter().getItemCount() -1 == itemPosition;
+        }
+        return false;
+    }
+    /**
+     * 是否最后span
+     */
+    public boolean isLastSpan(int itemPosition, RecyclerView parent) {
+        RecyclerView.LayoutManager layoutManager = parent.getLayoutManager();
+        if (layoutManager instanceof GridLayoutManager) {
+            int spanCount = ((GridLayoutManager) layoutManager).getSpanCount();
+            if ((itemPosition + 1) % spanCount == 0)
+                return true;
+        }
+        return false;
+    }
+
+}
